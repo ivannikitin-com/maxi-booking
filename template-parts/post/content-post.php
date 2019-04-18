7ca511d003130301e82336d2cbc3d5ebe38299a7
Vadim Mishkhozhev <vadim.stug@gmail.com>
2019-04-19 03:44:47 +0700

    fix post page

--- template-parts/post/content-post.php
+++ template-parts/post/content-post.php
@@ -1,5 +1,43 @@
 <?php
 
+$category_detail=get_the_category($post->ID);
+
+$quote      = get_field('quote');
+$photograph = get_field('photograph');
+$thumb      = get_field('thumb');
+?>
+
+<div class="container blog-post-top">
+	<div class="row">
+		<div class="col-lg-5">
+      <?php
+      if ( function_exists('yoast_breadcrumb') ) {
+			yoast_breadcrumb( '<div id="breadcrumbs">','</div>' );
+		  } ?>
+			<div class="category">
+        <?php
+          foreach($category_detail as $cd){
+            echo $cd->cat_name;
+          }
+        ?>
+      </div>
+			<?php the_title( '<h1>', '</h1>' ); ?>
+			<div class="author">
+        <?php echo get_avatar( get_the_author_meta( 'ID' ) ); ?>
+				<div class="info">
+					<div class="name"><?php the_author_lastname(); ?> <?php the_author_firstname(); ?></div>
+					<div class="desc"><?php the_author_description(); ?></div>
+				</div>
+			</div>
+			<div class="quote"><?php echo $quote; ?></div>
+		</div>
+		<div class="col-lg-7 image"><div class="desc photo-name"><?php echo $photograph; ?></div></div>
+		<?php echo $thumb; ?>
+	</div>
+</div>
+
+<?php
+
 include_once ABSPATH . 'wp-admin/includes/plugin.php';
 if ( is_plugin_active( 'advanced-custom-fields-pro/acf.php' ) ) :
 
